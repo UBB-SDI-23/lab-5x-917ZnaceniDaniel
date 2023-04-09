ef936c287704b91895bfffc07ccac08526c1e2f7
917ZnaceniDaniel <daniel.znaceni@stud.ubbcluj.ro>
2023-04-09 20:21:37 +0300

    commit

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -10,11 +10,6 @@ import { AppHome } from "./components/AppHome";
 import { AirportCreate } from "./components/AirportCreate";
 import { AirportDelete } from "./components/AirportDelete";
 import { AirportUpdate } from "./components/AirportUpdate";
-// import { TennisPlayerDetail } from './components/tennisPlayer/TennisPlayerDetail';
-// import { TennisPlayerDelete } from './components/tennisPlayer/TennisPlayerDelete';
-// import { TennisPlayerAdd } from './components/tennisPlayer/TennisPlayerAdd';
-// import { TennisPlayerUpdate } from './components/tennisPlayer/TennisPlayerUpdate';
-// import { TennisPlayerShowAvgExpCoach } from './components/tennisPlayer/TennisPlayerShowAvgExpCoach';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -32,11 +27,6 @@ function App() {
             element={<AirportDelete />}
           />
           <Route path="update-airport/:airportId" element={<AirportUpdate />} />
-          {/* <Route path="/tennisplayers/:tennisPlayerId" element={<TennisPlayerDetail />} />
-          <Route path="/tennisplayers/:tennisPlayerId/delete" element={<TennisPlayerDelete />} />
-          <Route path="/tennisplayers/add" element={<TennisPlayerAdd />} /> 
-          <Route path="/tennisplayers/:tennisPlayerId/edit" element={<TennisPlayerUpdate />} />
-          <Route path="/tennisplayers/avgyoecoaches" element={<TennisPlayerShowAvgExpCoach />} /> */}
         </Routes>
       </Router>
     </React.Fragment>
