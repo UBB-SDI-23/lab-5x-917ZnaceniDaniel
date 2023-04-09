f78e76a5f2ba1be129a6b86b277fd73add427ada
917ZnaceniDaniel <daniel.znaceni@stud.ubbcluj.ro>
2023-04-09 19:59:03 +0300

    Update constants.ts

--- react-frontend/src/constants.ts
+++ react-frontend/src/constants.ts
@@ -1,1 +1,10 @@
-export const BACKEND_API_URL ="https://ec2-13-50-245-162.eu-north-1.compute.amazonaws.com/api"
+// export const BACKEND_API_URL ="https://ec2-13-50-245-162.eu-north-1.compute.amazonaws.com/api"
+
+const PROD_BACKEND_API_URL =
+  "https://ec2-13-50-245-162.eu-north-1.compute.amazonaws.com/api";
+const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/api";
+
+export const BACKEND_API_URL =
+  process.env.NODE_ENV === "development"
+    ? DEV_BACKEND_API_URL
+    : PROD_BACKEND_API_URL;
