c0ffec8b052cb13edf6ac61773fdd6c954327ad9
917ZnaceniDaniel <daniel.znaceni@stud.ubbcluj.ro>
2023-04-09 22:50:05 +0300

    DEVELOPMENT nu fucking developement
    invata te sa scrii bag pl

--- react-frontend/src/constants.ts
+++ react-frontend/src/constants.ts
@@ -1,8 +1,7 @@
-const PROD_BACKEND_API_URL =
-  "/api";
-const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/api";
+const PROD_BACKEND_API_URL ="/api";
+const DEV_BACKEND_API_URL = "http://ec2-13-50-226-228.eu-north-1.compute.amazonaws.com/api";
 
 export const BACKEND_API_URL =
-  process.env.NODE_ENV === "developement"
+  process.env.NODE_ENV === "development"
     ? DEV_BACKEND_API_URL
     : PROD_BACKEND_API_URL;
