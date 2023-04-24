f1a863a667b3250e97b677f791ed1bb2499bcbc4
917ZnaceniDaniel <daniel.znaceni@stud.ubbcluj.ro>
2023-04-24 08:56:23 +0300

    ..
    trebuia sa adaug no_departing in serilizer

--- django-backend/base/serializers/AirportSerializer.py
+++ django-backend/base/serializers/AirportSerializer.py
@@ -3,6 +3,8 @@ from base.models.AirportModel import Airport
 
 
 class AirportSerializer(serializers.ModelSerializer):
+    no_departing = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = Airport
         fields = '__all__'
